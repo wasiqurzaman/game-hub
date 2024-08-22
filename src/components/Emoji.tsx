d5469e82d765699dc703d4838191df0a91e18842
Wasiqur Zaman <wasiqurzaman@gmail.com>
2024-08-22 15:36:09 +0530

    add emojis based on ratings

---
+++ src/components/Emoji.tsx
@@ -0,0 +1,21 @@
+import { Image, ImageProps } from "@chakra-ui/react";
+import bullsEye from "../assets/bulls-eye.webp";
+import meh from "../assets/meh.webp";
+import thumbsUp from "../assets/thumbs-up.webp";
+
+interface Props {
+  rating: number;
+}
+
+function Emoji({ rating }: Props) {
+  const emojiMap: { [key: number]: ImageProps } = {
+    3: { src: meh, alt: "meh", boxSize: "25px" },
+    4: { src: thumbsUp, alt: "recommended", boxSize: "25px" },
+    5: { src: bullsEye, alt: "exceptional", boxSize: "35px" },
+  };
+  if (rating < 3) return null;
+
+  return <Image {...emojiMap[rating]} marginTop={2} />;
+}
+
+export default Emoji;
